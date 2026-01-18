c3271696ab734aae7715e65edc2f258ee7e78357
Ahsan <ahsanan@outlook.com>
2026-01-18 23:34:28 +0600

    implemented axios for products

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,6 +1,24 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
 
 const Home = () => {
+    const [products, setProducts] = useState([]);
+    const [colors, setColors] = useState([]);
+    const [sizes, setSizes] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+       const fectchProducts = async () => {
+           try{
+               const response = await axios.get('http://e-commerce-laravel.test/api/products');
+               console.log("response is", response.data);
+           }catch(e){
+               console.log(e);
+           }
+       }
+
+       fectchProducts();
+    })
     return (
         <>
             <div className="Home">
