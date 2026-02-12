1ef33cb657f8f7bd1057bc414427b19635e176f0
Ahsan <ahsanan@outlook.com>
2026-02-12 14:24:16 +0600

    comment out of return span

--- src/components/products/Product.jsx
+++ src/components/products/Product.jsx
@@ -185,7 +185,7 @@ const Product = () => {
                 </div>
                 <div className="flex items-center gap-3 text-gray-600">
                   <RotateCcw className="w-5 h-5 text-indigo-600" />
-                  <span className="text-sm">30 Day Returns</span>
+                  {/*<span className="text-sm">30 Day Returns</span>*/}
                 </div>
               </div>
             </div>
